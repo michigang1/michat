b342b30067da9d78e164caeabb81a6b2b64237ad
Michael Chirozidi <maikl.tigriv@gmail.com>
2022-06-09 17:04:15 +0200

    -create variables , to which the IDs of the fields of the "Sign Up" layer are assigned -created Intent for btn 'Sign Up'

--- app/src/main/java/me/michigang1/michat/Login.kt
+++ app/src/main/java/me/michigang1/michat/Login.kt
@@ -1,11 +1,27 @@
 package me.michigang1.michat
-
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.Button
+import android.widget.EditText
 
 class Login : AppCompatActivity() {
+    private lateinit var editEmail: EditText
+    private lateinit var editPassword: EditText
+    private lateinit var btnLogin: EditText
+    private lateinit var btnSignUp: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        editEmail = findViewById(R.id.edit_email)
+        editPassword = findViewById(R.id.edit_password)
+        btnLogin = findViewById(R.id.btnLogin)
+        editEmail = findViewById(R.id.btnSignUp)
+
+        btnSignUp.setOnClickListener{
+            val intent = Intent(this, SignUp::class.java)
+        }
     }
 }
