402c9f3625d763f4d305e6f92d3e8440694b5fb7
Michael Chirozidi <maikl.tigriv@gmail.com>
2022-06-15 20:58:08 +0200

    the function UserAdapter was written using the Adapter Pattern

--- app/src/main/java/me/michigang1/michat/UserAdapter.kt
+++ app/src/main/java/me/michigang1/michat/UserAdapter.kt
@@ -1,5 +1,30 @@
 package me.michigang1.michat
 
-class UserAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class UserAdapter(val context: Context, val userList: ArrayList<User>): RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val view: View = LayoutInflater.from(context).inflate(R.layout.user_layout, parent, false)
+        return UserViewHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val currentUser = userList[position]
+
+        holder.textName.text = currentUser.name
+    }
+    override fun getItemCount(): Int {
+        return userList.size
+    }
+
+    class UserViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val textName = itemView.findViewById<TextView>(R.id.txt_name)
+    }
 
 }
