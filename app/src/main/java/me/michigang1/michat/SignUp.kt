8b3990ffbf4b5f0a2976bd9fd2c9c3a6f5e1bf64
Michael Chirozidi <maikl.tigriv@gmail.com>
2022-06-20 00:38:25 +0200

    Firebase support has been rebuilt due to the bug (check issue) Also some refactoring

--- app/src/main/java/me/michigang1/michat/SignUp.kt
+++ app/src/main/java/me/michigang1/michat/SignUp.kt
@@ -63,6 +63,6 @@ class SignUp : AppCompatActivity() {
 
     private fun addUserToDatabase( name: String, email: String, uid: String){
         mDbRef = FirebaseDatabase.getInstance().reference
-        mDbRef.child("user").child(uid).setValue(User(name, email, uid) )
+        mDbRef.child("users").child("user: $uid").setValue(User(name, email, uid))
     }
 }
