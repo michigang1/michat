b01fe4f59f20c8049ad469652a57b22aae6174f7
Michael Chirozidi <maikl.tigriv@gmail.com>
2022-06-26 22:00:39 +0200

    Added support of RecyclerView of messages and Google Firebase Auth and Dbase; also some refactoring

--- app/src/main/java/me/michigang1/michat/ChatActivity.kt
+++ app/src/main/java/me/michigang1/michat/ChatActivity.kt
@@ -4,15 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
-import android.widget.ImageButton
 import android.widget.ImageView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class ChatActivity : AppCompatActivity() {
 
-    private lateinit var chatRecyclerView: RecyclerView
+    private lateinit var messageRecyclerView: RecyclerView
     private lateinit var messageBox: EditText
+    private lateinit var messageAdapter: MessageAdapter
+    private lateinit var messageList: ArrayList<Message>
     private lateinit var btnSend: ImageView
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDbRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,12 +27,25 @@ class ChatActivity : AppCompatActivity() {
 
         val intent = Intent()
         val name = intent.getStringExtra("name")
-        val uid = intent.getStringExtra("uid")
+        val receiverUid = intent.getStringExtra("uid")
 
         supportActionBar?.title = name
 
-        chatRecyclerView = findViewById(R.id.chatRecyclerView)
+        mAuth = FirebaseAuth.getInstance()
+        mDbRef = FirebaseDatabase.getInstance().reference
+
         messageBox = findViewById(R.id.messageBox)
         btnSend = findViewById(R.id.btnSend)
+        messageRecyclerView = findViewById(R.id.messageRecyclerView)
+
+        messageList = ArrayList()
+        messageAdapter = MessageAdapter(this, messageList)
+        messageRecyclerView.run {
+            layoutManager = LinearLayoutManager(this@ChatActivity)
+            adapter = messageAdapter
+        }
+
+
+
     }
 }
